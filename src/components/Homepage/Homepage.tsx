b7ae33566dd4dd0a38d60ad116040990b44fa127
Romal-2303 <angryrocket789@gmail.com>
2025-03-03 13:22:01 +0530

    Moved about us folder inside homepage

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,10 +1,12 @@
 import MainSection from "./MainSection/MainSection";
 import classes from "./Homepage.module.scss";
+import AboutUs from "./AboutUs/AboutUs";
 
 const Homepage = () => {
   return (
     <div className={classes["homepage-container"]}>
       <MainSection />
+      <AboutUs />
     </div>
   );
 };
